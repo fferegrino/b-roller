f3979ea4aea3a8b4e3f96980c46c8fc98eeb6464
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-11-05 17:37:27 +0000

    major refactoring

--- editingpal/__main__.py
+++ editingpal/__main__.py
@@ -1,31 +1,20 @@
 import typer
-from pytube import YouTube
-from slugify import slugify
 
-from editingpal.giphy.giphy import download_giphy
-from editingpal.pixabay.pixabay import download_pixabay_video
+from editingpal.giphy import download_giphy
+from editingpal.pixabay import download_pixabay_video
+from editingpal.youtube import download_youtube_audio, download_youtube_video
 
 app = typer.Typer(add_completion=False)
 
 
 @app.command()
 def video(url: str):
-    yt = YouTube(url)
-    title = slugify(yt.title)
-    streams = yt.streams.filter(only_video=True)
-    streams = [(stream.itag, int(stream.resolution[:-1])) for stream in streams if stream.mime_type == "video/mp4"]
-    max_res_itag = sorted(streams, key=lambda itags: itags[1], reverse=True)[0][0]
-    yt.streams.get_by_itag(max_res_itag).download(filename=f"{title}_video.mp4")
+    download_youtube_video(url)
 
 
 @app.command()
 def audio(url: str):
-    yt = YouTube(url)
-    title = slugify(yt.title)
-    streams = yt.streams.filter(only_audio=True)
-    mp4_streams = [stream for stream in streams if stream.mime_type == "audio/mp4"]
-    streams = sorted(mp4_streams, key=lambda stream: int(stream.abr[:-4]), reverse=True)
-    streams[0].download(filename=f"{title}_audio.mp4")
+    download_youtube_audio(url)
 
 
 @app.command()
@@ -34,7 +23,7 @@ def pixabay(url: str):
 
 
 @app.command()
-def gif(url: str):
+def giphy(url: str):
     download_giphy(url)
 
 
---
+++ editingpal/youtube.py
@@ -0,0 +1,25 @@
+def get_streams(url, only_video):
+    yt = YouTube(url)
+    title = slugify(yt.title)
+    args = {}
+    if only_video:
+        args["only_video"] = True
+        mime_type = "video/mp4"
+    else:
+        args["only_audio"] = True
+        mime_type = "audio/mp4"
+
+    streams = [stream for stream in yt.streams.filter(**args) if stream.mime_type == mime_type]
+    return streams, title
+
+
+def download_youtube_video(url):
+    streams, title = get_streams(url, only_video=True)
+    streams = sorted(streams, key=lambda stream: int(stream.resolution[:-1]), reverse=True)
+    streams[0].download(filename=f"{title}_video.mp4")
+
+
+def download_youtube_audio(url):
+    streams, title = get_streams(url, only_video=False)
+    streams = sorted(streams, key=lambda stream: int(stream.abr[:-4]), reverse=True)
+    streams[0].download(filename=f"{title}_audio.mp4")
