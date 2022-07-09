96d2bbe979cd6c05a783a472f3d5e0eced63b93a
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-07-09 12:06:54 +0100

    modify youtube api

--- asset_downloader/youtube.py
+++ asset_downloader/youtube.py
@@ -1,29 +1,37 @@
 from pytube import YouTube
+from pytube.streams import Stream
 from slugify import slugify
 
+video_itags = [137, 136, 135, 134, 133, 160]
+audio_itags = [140, 139]
 
-def get_streams(url, only_video):
-    yt = YouTube(url)
-    title = slugify(yt.title)
+
+def get_streams(video: YouTube, kind: str) -> Stream:
     args = {}
-    if only_video:
+    if kind == "video":
         args["only_video"] = True
         mime_type = "video/mp4"
-    else:
+        streams = [stream for stream in video.streams.filter(**args) if stream.mime_type == mime_type]
+        sorted_streams = sorted(
+            streams, key=lambda stream: 1000 if stream.itag not in video_itags else video_itags.index(stream.itag)
+        )
+    elif kind == "audio":
         args["only_audio"] = True
         mime_type = "audio/mp4"
+        streams = [stream for stream in video.streams.filter(**args) if stream.mime_type == mime_type]
+        sorted_streams = sorted(
+            streams, key=lambda stream: 1000 if stream.itag not in audio_itags else audio_itags.index(stream.itag)
+        )
 
-    streams = [stream for stream in yt.streams.filter(**args) if stream.mime_type == mime_type]
-    return streams, title
-
-
-def download_youtube_video(url, **kwargs):
-    streams, title = get_streams(url, only_video=True)
-    streams = sorted(streams, key=lambda stream: int(stream.resolution[:-1]), reverse=True)
-    streams[0].download(filename=f"{title}_video.mp4")
+    return sorted_streams[0]
 
 
-def download_youtube_audio(url, **kwargs):
-    streams, title = get_streams(url, only_video=False)
-    streams = sorted(streams, key=lambda stream: int(stream.abr[:-4]), reverse=True)
-    streams[0].download(filename=f"{title}_audio.mp4")
+def download_video(video_id: str):
+    video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+    name_slug = slugify(video.title)
+
+    audio = get_streams(video, "audio")
+    video = get_streams(video, "video")
+
+    audio.download(filename=f"{name_slug}__{video_id}__audio.mp4")
+    video.download(filename=f"{name_slug}__{video_id}__video.mp4")
