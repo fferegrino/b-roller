6cfbcea37016dfc1a89cc014cd7c4b3d08e0a2af
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-07-10 07:31:48 +0100

    Merge videos using ffmpeg

--- asset_downloader/youtube.py
+++ asset_downloader/youtube.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import ffmpeg
 from pytube import YouTube
 from pytube.streams import Stream
@@ -27,21 +29,35 @@ def get_streams(video: YouTube, kind: str) -> Stream:
     return sorted_streams[0]
 
 
-def download_video(video_id: str):
+def download_video(video_id: str, start_time: Optional[str] = None, end_time: Optional[str] = None) -> None:
     video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
     name_slug = slugify(video.title)
+
+    original_name = f"{name_slug}__{video_id}"
+    audio_file = f"{original_name}_audio.mp4"
+    video_file = f"{original_name}_video.mp4"
 
     audio = get_streams(video, "audio")
     video = get_streams(video, "video")
 
-    original_name = f"{name_slug}__{video_id}"
-
-    audio_file = f"{original_name}_audio.mp4"
-    video_file = f"{original_name}_video.mp4"
-
     audio.download(filename=audio_file)
     video.download(filename=video_file)
 
-    input_video = ffmpeg.input(video_file)
-    input_audio = ffmpeg.input(audio_file)
+    ffmpeg_processing(audio_file, video_file, original_name, end_time, start_time)
+
+
+def ffmpeg_processing(
+    audio_file: str,
+    video_file: str,
+    original_name: str,
+    end_time: Optional[str] = None,
+    start_time: Optional[str] = None,
+) -> None:
+    ffmpeg_arguments = {}
+    if start_time:
+        ffmpeg_arguments["ss"] = start_time
+    if end_time:
+        ffmpeg_arguments["to"] = end_time
+    input_video = ffmpeg.input(video_file, **ffmpeg_arguments)
+    input_audio = ffmpeg.input(audio_file, **ffmpeg_arguments)
     ffmpeg.concat(input_video, input_audio, v=1, a=1).output(f"./{original_name}.mp4").run()
